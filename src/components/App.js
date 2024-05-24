add36baf142f3a38cab4c45c4be6e3a5639ae175
ahmadqaranyfarhan <ahmadqaranyfarhan5@gmail.com>
2024-05-24 16:41:00 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+  const [Loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogImage(data.message);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching dog image:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      {Loading ? <p>Loading...</p> : <img src={dogImage} alt="A Random Dog" />}
+    </div>
+  );
+}
+
+export default App;
